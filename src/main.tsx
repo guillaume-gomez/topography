3bcf34459e7152548f8a45e5725c4c37b2ee7183
Guillaume Gomez <guillaume.gomez846@gmail.com>
2026-03-24 08:39:15 +0100

    add component color-palette ui

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css';
 import App from './App.tsx';
+import ChooseColor from "./ChooseColor";
 
 import SettingsContextWrapper from "./components/SettingsContextWrapper";
 
@@ -9,6 +10,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <SettingsContextWrapper>
       <App />
+      <ChooseColor onSubmit={() => console.log("fdkjd")} />
     </SettingsContextWrapper>
   </StrictMode>,
 )
