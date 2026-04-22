f20801ca6b58a6130347a23d83b1932dcdbe11a7
Guillaume Gomez <guillaume.gomez846@gmail.com>
2026-04-22 08:42:42 +0200

    create new number input + insert it in the choose color component

---
+++ src/components/NumberInput.tsx
@@ -0,0 +1,31 @@
+
+interface NumberInputProps {
+  label: string;
+  value: number;
+  min: number;
+  max:number
+  step?: number;
+  onChange: (newValue: number) => void;
+  size?: string;
+}
+
+function NumberInput({label, onChange, value, min, max, step = 1, size = "input-xs"} : NumberInputProps) {
+  return (
+    <div className="form-control">
+      <label>{label}</label>
+      <input
+        type="number"
+        className={`input validator ${size}`}
+        placeholder="Type a number"
+        step={step}
+        min={min}
+        max={max}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      />
+      {min && max && <p className="validator-hint">{`Must be between be ${min} to ${max}`}</p>}
+    </div>
+  );
+}
+
+export default NumberInput;
