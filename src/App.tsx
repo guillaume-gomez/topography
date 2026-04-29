d69c8a28ef32c61fbbde981c22be945a55a6094d
Guillaume Gomez <guillaume.gomez846@gmail.com>
2026-04-29 14:11:33 +0200

    start using usettransition to manage scenes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, type CSSProperties } from 'react';
 import { SettingsContext } from "./context/SettingsContextWrapper";
 import { SceneContext } from "./context/SceneContextWrapper";
-import { useSpring, useSpringRef, animated, easings } from '@react-spring/web';
+import { useSpring, useSpringRef, animated, easings, useTransition, type AnimatedProps } from '@react-spring/web';
 
 import ChooseColor from "./ChooseColor";
 import ThreejsRenderer from './components/threeJs/ThreeJsRenderer';
 import useTopography from "./components/hooks/useTopography";
 import Card from "./components/Card";
 import ParallaxTilt from "./components/ParallaxTilt";
+
+type AnimationProps = AnimatedProps<CSSProperties>
 
 function App() {
   const {
@@ -28,13 +30,12 @@ function App() {
     isColorChoose,
     isIntro,
     is3DScene,
-    animationIntroEnd,
     animationColorChoiceEnd,
     setAnimationEnd
   } = useContext(SceneContext);
 
   const { generate, shapes } = useTopography({
-    width, 
+    width,
     height,
     numberOfLayers,
     fromToColors: [colorFrom, colorTo]
@@ -47,39 +48,42 @@ function App() {
   }, [isColorChoose])
 
   const apiTransitionIntro = useSpringRef();
-  const transitionIntroProps  = useSpring(
+  const transitionIntroProps  = useTransition(
+      isIntro(),
       {
         ref: apiTransitionIntro,
-        from: { position: "relative", top: "0%" },
-        to: [
-          { position: "relative", top: "-300%" },
-          { position: "relative" },
+        from: { position: "relative", top: "0%", display: "flex" },
+        enter: [
+          { position: "relative", top: "-300%", display: "flex" },
+          { position: "static", top: "-300%", display: "none" }
         ],
+        //leave: { position: "relative", top: "-300%", display: "none" },
         config: { duration: 500, easing: easings.easeInBack },
         onStart: () => {
           setSceneName("color-choice");
         },
-        onRest: (result, spring, item) => {
-          //apiTransitionThreeJsRenderer.start();
+        onRest: () => {
           setAnimationEnd("intro", true);
         }
       }
   );
 
   const apiTransitionChooseColor = useSpringRef();
-  const transitionChooseColorProps  = useSpring(
+  const transitionChooseColorProps  = useTransition(
+      isColorChoose(),
       {
         ref: apiTransitionChooseColor,
-        from: { position: "relative", top: "0%" },
-        to: [
-          { position: "relative", top: "-200%" },
-          { position: "relative" },
+        from: { position: "relative", top: "0%", display: "flex " },
+        enter: { position: "relative", top: "200%", display: "flex" },
+        leave: [
+          { position: "relative", top: "-200%", display: "flex" },
+          { position: "relative", display: "none" },
         ],
         config: { duration: 500, easing: easings.easeInBack },
         onStart: () => {
           setSceneName("3d-scene");
         },
-        onRest: (result, spring, item) => {
+        onRest: () => {
           apiTransitionThreeJsRenderer.start();
           setAnimationEnd("color-choice", true);
         }
@@ -87,11 +91,12 @@ function App() {
   );
 
   const apiTransitionThreeJsRenderer = useSpringRef();
-  const transitionThreeJsRendererProps  = useSpring(
+  const transitionThreeJsRendererProps  = useTransition(
+      is3DScene(),
       {
         ref: apiTransitionThreeJsRenderer,
         from: { position: "relative", opacity: 0.3, top: "-210%" },
-        to: [
+        enter: [
           { position: "relative", opacity: 1, top: "0%" },
           { position: "static", opacity: 1, top: "0%" }
         ],
@@ -106,46 +111,53 @@ function App() {
   return (
     <>
       <div className="
-        absolute -z-10 inset-0 h-full w-full 
-        bg-[linear-gradient(to_right,#73737320_1px,#03030349_1px),linear-gradient(to_bottom,#73737320_1px,#03030349_1px)] 
+        absolute -z-10 inset-0 h-full w-full
+        bg-[linear-gradient(to_right,#73737320_1px,#03030349_1px),linear-gradient(to_bottom,#73737320_1px,#03030349_1px)]
         bg-[size:30px_30px]"
       />
       <div className="w-screen h-screen p-5 flex">
-        
-        <animated.div className="w-full h-full p-5 items-center justify-center" style={{...transitionIntroProps, display: isIntro() || !animationIntroEnd ? "flex" : "none"}}>
-          <ParallaxTilt/>      
-        </animated.div>
-        
-        <animated.div 
+        {
+          transitionIntroProps(style => (
+            <animated.div className="w-full h-full p-5 items-center justify-center" style={style as AnimationProps}>
+              <ParallaxTilt/>
+            </animated.div>
+          ))
+        }
+        {
+          transitionChooseColorProps(style => (
+            <animated.div
             className="w-full"
-            style={{
-              ...transitionChooseColorProps,
-              display: isColorChoose() && !animationColorChoiceEnd ? "block" : "none"
-            }}
-        >
-          <ChooseColor onSubmit={(colorFrom, colorTo, layers) => {
-            // Handle the color submission
-            setColorFrom(colorFrom);
-            setColorTo(colorTo);
-            setNumberOfLayers(layers);
+            style={style as AnimationProps}
+            >
+              <ChooseColor onSubmit={(colorFrom, colorTo, layers) => {
+                // Handle the color submission
+                setColorFrom(colorFrom);
+                setColorTo(colorTo);
+                setNumberOfLayers(layers);
 
-            apiTransitionChooseColor.start();
-          }} />
-        </animated.div>
-        <animated.div
-          className="w-full h-screen"
-          style={{...transitionThreeJsRendererProps, display: is3DScene() ? "block" : "none"}}
-        >
-          <Card kustomClass="absolute left-10 top-10 z-10 opacity-70">
-            <button className="btn btn-primary" onClick={() => {generate(); setAnimationState("started")}}>
-              Generate
-            </button>
-            <button className="btn btn-xs btn-secondary" onClick={() => setLight(!isLight)}>
-              {isLight ? "Light" : "Dark"}
-            </button>
-          </Card>
-          <ThreejsRenderer shapes={shapes}/> 
-        </animated.div> 
+                apiTransitionChooseColor.start();
+              }} />
+            </animated.div>
+          ))
+        }
+        {
+          transitionThreeJsRendererProps(style => (
+            <animated.div
+              className="w-full h-screen"
+              style={style as AnimationProps}
+            >
+              <Card kustomClass="absolute left-10 top-10 z-10 opacity-70">
+                <button className="btn btn-primary" onClick={() => {generate(); setAnimationState("started")}}>
+                  Generate
+                </button>
+                <button className="btn btn-xs btn-secondary" onClick={() => setLight(!isLight)}>
+                  {isLight ? "Light" : "Dark"}
+                </button>
+              </Card>
+              <ThreejsRenderer shapes={shapes}/>
+            </animated.div>
+          ))
+        }
       </div>
     </>
   )
