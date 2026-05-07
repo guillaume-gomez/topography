35955e97c33a93743e543cc4a2396aebfb3f7d7c
Guillaume Gomez <guillaume.gomez846@gmail.com>
2026-05-07 09:04:54 +0200

    update mobile design

--- src/App.tsx
+++ src/App.tsx
@@ -85,7 +85,7 @@ function App() {
         bg-[linear-gradient(to_right,#73737320_1px,#03030349_1px),linear-gradient(to_bottom,#73737320_1px,#03030349_1px)]
         bg-[size:30px_30px]"
       />
-      <div className="w-screen h-screen p-5 flex flex-col">
+      <div className="w-screen h-screen md:p-5 p-2 flex flex-col">
         {
           transitionIntroProps(style => (
             <animated.div className="w-full h-full p-5 items-center justify-center" style={style as AnimationProps}>
@@ -116,7 +116,7 @@ function App() {
               className="w-full h-screen"
               style={style as AnimationProps}
             >
-              <Card kustomClass="absolute left-10 top-10 z-10 opacity-70">
+              <Card kustomClass="absolute left-2 lg:left-5  top-2 lg:top-5 z-10 opacity-70">
                 <button className="btn btn-primary" onClick={onGenerate}>
                   Generate
                 </button>
