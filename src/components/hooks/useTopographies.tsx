32dc375fd3d5878858a14f074d0b8d516f913bca
Guillaume Gomez <guillaume.gomez846@gmail.com>
2026-05-13 14:07:08 +0200

    taking account min max

--- src/components/hooks/useTopographies.tsx
+++ src/components/hooks/useTopographies.tsx
@@ -70,8 +70,8 @@ function useTopographies({ width, height, numberOfLayers, fromToColors } : Topog
 
   async function computeGrid(): Grid {
     if(loadDirectFile) {
-      const { width, height, values } = await getData(`${BASE_URL}/presets/vesuvio.json`);
-      return { gridWidth: width, gridHeight: height, data: values, min: 0, max: 1252 };
+      const { width, height, values, min, max } = await getData(`${BASE_URL}/presets/mont-blanc.json`);
+      return { gridWidth: width, gridHeight: height, data: values, min, max };
     }
 
     // fallback generate noise to create a grid
